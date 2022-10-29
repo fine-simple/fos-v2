2bbb21d49404f568d119d760c1d9224248fd0337
Ahmed Tawfik <at200000000@gmail.com>
2022-10-29 23:32:02 +0200

    [Finished] initialize_MemBlockList

--- lib/dynamic_allocator.c
+++ lib/dynamic_allocator.c
@@ -60,10 +60,13 @@ void print_mem_block_lists()
 //===============================
 void initialize_MemBlocksList(uint32 numOfBlocks)
 {
-	//TODO: [PROJECT MS1] [DYNAMIC ALLOCATOR] initialize_MemBlocksList
-	// Write your code here, remove the panic and write your code
-	panic("initialize_MemBlocksList() is not implemented yet...!!");
-
+	// TODO: [PROJECT MS1] [DYNAMIC ALLOCATOR] initialize_MemBlocksList
+	//  Write your code here, remove the panic and write your code
+	LIST_INIT(&AvailableMemBlocksList);
+	for (int i = 0; i < numOfBlocks; i++)
+	{
+		LIST_INSERT_HEAD(&AvailableMemBlocksList, &(MemBlockNodes[i]));
+	}
 }
 
 //===============================
